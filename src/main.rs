c1d9837cc156381580d9052b60100ac95049eb97
Philippe Llerena <philippe.llerena@forticheprod.com>
2025-02-26 13:31:21 +0100

    Update dependencies and refactor main.rs to include InfluxDB derive macros for temperature data

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use serde::{Deserialize, Serialize};
+use chrono::Utc;
 use futures::prelude::*;
-use influxdb2::models::DataPoint;
 use influxdb2::Client;
+use influxdb2_derive::WriteDataPoint;
+use serde::{Deserialize, Serialize};
 use w1_therm_reader::{convert_to_metric, read_from_file};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -25,19 +26,30 @@ impl Default for ConfyConfig {
     }
 }
 
+#[derive(WriteDataPoint, Debug)]
+#[measurement = "cpu_load_short"]
+struct Temperature {
+    #[influxdb(tag)]
+    location: Option<String>,
+    #[influxdb(field)]
+    value: f64,
+    #[influxdb(timestamp)]
+    time: i64,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cfg: ConfyConfig = confy::load("salon_prob", None)?;
     let file = confy::get_configuration_file_path("salon_prob", None)?;
     println!("The configuration file path is: {:#?}", file);
-    let t = read_from_file(&cfg.bus_file).unwrap();
-    let t_metric: f64 = convert_to_metric(t).into();
     let client = Client::new(cfg.influx_host, cfg.influx_org, cfg.influx_token);
-
-    let points = vec![DataPoint::builder("salon")
-        .field("Temperature", t_metric)
-        .build()?];
-
+    let t = read_from_file(&cfg.bus_file).unwrap();
+    let t: Temperature = Temperature {
+        location: Some("Salon".to_string()),
+        value: convert_to_metric(t).into(),
+        time: Utc::now().timestamp_nanos_opt().expect("Can get timestamp"),
+    };
+    let points: Vec<Temperature> = vec![t];
     client
         .write(&cfg.influx_bucket, stream::iter(points))
         .await?;
